f6f3c49951e3f8c150395fd0267d733ec9917a6a
shinhyew <she8729me@naver.com>
2024-04-04 16:10:40 +0900

    homework4-1

---
+++ 4주차/padding.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ struct student //구조체 student 선언
+ {
+ char lastName[13];   /* 13 bytes */
+ int studentId;       /* 4 bytes */
+ short grade;         /* 2 bytes */
+ };
+ int main()
+ {
+    printf("----- [신혜원]-----\n");
+    struct student pst;
+    printf("size of student = %ld\n", sizeof(struct student)); //구조체 student의 크기 출력 합쳐서 20바이트 이지만, padding으로 인해 24바이트 출력
+    printf("size of int     = %ld\n", sizeof(int)); //int의 크기 출력
+    printf("size of short   = %ld\n", sizeof(short)); //short의 크기 출력
+    return 0;
+ }
+ 
